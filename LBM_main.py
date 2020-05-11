4a657ea2c67171dcce7ed45806f097764842d915
SharathTUdelft <43334570+SharathTUdelft@users.noreply.github.com>
2020-05-11 23:06:38 +0530

    changed initilization of f

--- LBM_main.py
+++ LBM_main.py
@@ -53,8 +53,8 @@ f_eq = np.ones((height, width, 9)) # equilibrium number density
 
 #----- initalizing macroscopic quantities
 rho = np.ones((height, width))
-ux = (f[:,:,2] + f[:,:,5] + f[:,:,8] - (f[:,:,0] + f[:,:,3] + f[:,:,6])) / rho # eq.(7)
-uy = (f[:,:,0] + f[:,:,1] + f[:,:,2] - (f[:,:,6] + f[:,:,7] + f[:,:,8])) / rho
+ux = (flow[:,:,1] + flow[:,:,5] + flow[:,:,8] - (flow[:,:,3] + flow[:,:,6] + flow[:,:,7])) / rho
+uy = (flow[:,:,2] + flow[:,:,5] + flow[:,:,6] - (flow[:,:,4] + flow[:,:,7] + flow[:,:,8])) / rho
 u = np.sqrt(ux**2 + uy**2)
 
 #------ initialize borders ------
